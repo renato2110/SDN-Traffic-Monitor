975f530320e13691abc41accae878c7fe2e4fd3a
renato2110 <renato.mainieri2110@gmail.com>
2018-10-24 16:03:49 -0600

    Repository Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './components/example/example.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
