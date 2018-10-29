4596fc23e5a1255e441572d8141ef9d23ce6d56b
renato.mainieri <renato.mainieri2110@gmail.com>
2018-10-29 16:14:47 -0600

    Components creation

--- src/app/components/dns/dns.component.ts
+++ src/app/components/dns/dns.component.ts
@@ -18,14 +18,14 @@ export class DnsComponent implements OnInit {
           "mac": "99:12:aa:01:ff:11",
           "port": "1",
           "ip": "10.0.0.1",
-          "destiny_ip": "192.168.10.1",
+          "destination_ip": "192.168.10.1",
           "bytes": "324"
       },  
       {
           "mac": "10:AB:B5:22:01",
           "port": "2",
           "ip": "192.168.10.1",
-          "destiny_ip": "10.0.0.1",
+          "destination_ip": "10.0.0.1",
           "bytes": "1002"
       }
     ]
