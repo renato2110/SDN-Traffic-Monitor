670f03d12cde577a8d67b5b64a14d70e3aef310b
renato.mainieri <renato.mainieri2110@gmail.com>
2018-10-29 15:34:01 -0600

    Components creation

--- src/app/components/dns/dns.component.ts
+++ src/app/components/dns/dns.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DnsComponent implements OnInit {
 
+  private data;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.data = [
+      {
+          "mac": "99:12:aa:01:ff:11",
+          "port": "1",
+          "ip": "10.0.0.1",
+          "destiny_ip": "192.168.10.1",
+          "bytes": "324"
+      },  
+      {
+          "mac": "10:AB:B5:22:01",
+          "port": "2",
+          "ip": "192.168.10.1",
+          "destiny_ip": "10.0.0.1",
+          "bytes": "1002"
+      }
+    ]
+
   }
 
 }
